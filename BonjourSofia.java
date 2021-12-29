d54f5036ccadc97ba176ff1649c2e47a0bebd2a4
sofiav132 <sv132sv132@gmail.com>
2021-12-29 10:42:50 +0700

    change code SOUT

--- BonjourSofia.java
+++ BonjourSofia.java
@@ -1,5 +1,7 @@
 class BonjourSofia{
-public static void main(String[] args){
-System.out.println("Sofia: Java Application Class 3SC3_2021");
-}
+    public static void main(String[] args){
+        System.out.println("Title");
+        System.out.println("Sofia: Java Application Class 3SC3_2021");
+        System.out.println("--- *** ---");
+    }
 }
